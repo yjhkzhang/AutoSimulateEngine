298d37885858ede5204e5077bee5f79a9a701b10
yjhkzhang <yuanjia.zhang@outlook.com>
2022-05-10 09:58:31 +0800

    add events to imgui

--- AutoSimulate/src/AutoSimulate/Events/Event.h
+++ AutoSimulate/src/AutoSimulate/Events/Event.h
@@ -15,7 +15,7 @@ namespace AutoSimulate {
 		None = 0,
 		WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
 		AppTick, AppUpdate, AppRender,
-		KeyPressed, KeyReleased,
+		KeyPressed, KeyReleased, KeyTyped,
 		MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
 	};
 
--- AutoSimulate/src/AutoSimulate/ImGui/ImGuiLayer.h
+++ AutoSimulate/src/AutoSimulate/ImGui/ImGuiLayer.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "Layer.h"
+#include "Events/ApplicationEvent.h"
+#include "Events/KeyEvent.h"
+#include "Events/MouseEvent.h"
 
 namespace AutoSimulate {
 
@@ -14,6 +17,15 @@ namespace AutoSimulate {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	private:
 		float m_Time = 0.0f;
 	};
